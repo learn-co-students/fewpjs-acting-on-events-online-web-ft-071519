92cbd46d20cce9e7a426c858e01339960402350c
Ahmed Abououf <aabuauf@yahoo.com>
2019-10-15 19:52:30 -0400

    Done.

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ document.addEventListener("keydown", function (e) {
 
 
 function moveDodgerRight() {
-    let rightNumbers = dodger.style.right.replace("px", "");
+    let rightNumbers = dodger.style.left.replace("px", "");
     let right = parseInt(rightNumbers, 10);
 
     if (right < 360) {
-        dodger.style.right = `${right + 1}px`;
+        dodger.style.left = `${right + 1}px`;
     }
 }
 
