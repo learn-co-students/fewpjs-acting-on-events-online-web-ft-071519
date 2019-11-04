cc5b9f625f77cc94985620c1d8d5ff96f71a0748
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-11-04 19:37:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code here
+function moveDodgerRight() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left >= -5 && left < 360 ) {
+    dodger.style.left = `${left + 3}px`;
+  }
+  if (left > 360) {
+		dodger.style.left = "360px"
+  }
+}
